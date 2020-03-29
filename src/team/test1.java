162244e01826ce8cb8c7084415700dea4d56ffca
Mazin-hub <1285543153@qq.com>
2020-03-29 17:29:55 +0800

    Mzp-committest

--- src/team/test1.java
+++ src/team/test1.java
@@ -4,4 +4,8 @@ public class test1 {
     public static void Print(){
         System.out.println(1);
     }
+    private static void ai(){
+        Print();
+        System.out.println(2);
+    }
 }
