c3015d4bccf1e5657426af9563cfca5f8eeb06ce
Mazin-hub <1285543153@qq.com>
2020-03-29 15:12:02 +0800

    第一次马志鹏提交

--- src/team/main.java
+++ src/team/main.java
@@ -2,10 +2,7 @@ package team;
 
 public class main {
     public static void main(String[] args) {
-        System.out.println(1);
-        System.out.println("mzp牛逼");
-        System.out.println("mdl牛逼");
-        System.out.println("麦狄龙大佬12");
-        System.out.println("asd???????? ");
+        System.out.println("Mzp的分支");
+
     }
 }
