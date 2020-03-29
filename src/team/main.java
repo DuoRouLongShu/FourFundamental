349886b05c1b617a4c877db7e50e1090244d0b40
Mazin-hub <1285543153@qq.com>
2020-03-29 14:33:21 +0800

    Mzp-commit

--- src/team/main.java
+++ src/team/main.java
@@ -4,5 +4,6 @@ public class main {
     public static void main(String[] args) {
         System.out.println(1);
         System.out.println("mzp牛逼");
+        System.out.println("mdl牛逼");
     }
 }
